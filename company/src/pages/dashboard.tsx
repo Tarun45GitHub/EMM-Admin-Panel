65ef4787c8d50449e6ff181c8eb019119d454767
Tarun45GitHub <dalberatarun1@gmail.com>
2026-04-07 09:41:23 +0530

    debug

--- company/src/pages/dashboard.tsx
+++ company/src/pages/dashboard.tsx
@@ -6,42 +6,6 @@ import Chartpannel from "../components/dashboard/Graph_div";
 import CarouselBanner from "../components/dashboard/CarouselBanner";
 
 const Dashboard:React.FC=()=>{
-  // const { showLoader, hideLoader } = useLoader();
-  // const [data, setData] = useState<any>(null);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       showLoader();
-
-  //       // Use .getItem() - it's more reliable
-  //       const myToken = window.localStorage.token;
-
-  //       if (!myToken) {
-  //         console.warn("Token missing. Redirecting to login...");
-  //         // window.location.href = "/login"; 
-  //         return;
-  //       }
-
-  //       const response = await axios.get("https://backend.bharatemm.com/api/crm/dashboard/", {
-  //         headers: {
-  //           "Authorization": `Bearer ${myToken}`,
-  //           "Content-Type": "application/json"
-  //         }
-  //       });
-  //       const data=response?.data?.data;
-
-
-  //     } catch (error: any) {
-  //       if (error.response?.status === 401) {
-  //         console.error("Token is invalid or expired.");
-  //         // Optional: clear storage and logout user
-  //       }
-  //     } finally {
-  //       hideLoader();
-  //     }
-  //   };
-  //  // fetchData();
-  // }, []);
    return(
    <div className="dark:bg-[#1E293B]">
     <div className="p-5  rounded-lg">
